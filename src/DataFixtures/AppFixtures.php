3a74d3467760fde655acb73e7556e7bb4dae15d9
David <davitrons@gmail.com>
2022-05-10 00:10:05 +0200

    Generado fixtures a partir de los Factorys creados anteriormente

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\HistorialFactory;
+use App\Factory\LocalizacionFactory;
+use App\Factory\MaterialFactory;
+use App\Factory\PersonaFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,41 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        LocalizacionFactory::createMany(30, function (){
+            return[
+                'padre' => LocalizacionFactory::faker()->boolean() ? LocalizacionFactory::random() : null
+            ];
+        });
+
+        MaterialFactory::createMany(40, function (){
+            return[
+                'padre' => MaterialFactory::faker()->boolean() ? MaterialFactory::random() : null,
+                'localizacion' => LocalizacionFactory::random(),
+                'persona' => MaterialFactory::faker()->boolean() ? PersonaFactory::random() : null,
+                'prestadoPor' => MaterialFactory::faker()->boolean() ? PersonaFactory::random() : null,
+                'responsable' => MaterialFactory::faker()->boolean() ? PersonaFactory::random() : null,
+            ] ;
+        });
+
+        $administrador = PersonaFactory::new()->create([
+            'nombre' => 'Administrador',
+            'apellidos' => 'admin',
+            'administrador' => 'true',
+            'gestorPrestamos' => 'true',
+            'nombreUsuario' => 'Admin',
+            'clave' => 'Admin',
+        ]);
+        PersonaFactory::createMany(20);
+
+
+        HistorialFactory::createMany(30, function (){
+           return[
+               'material' => MaterialFactory::random(),
+               'prestadoPor' => PersonaFactory::random(),
+               'prestadoA' => PersonaFactory::random(),
+               'devueltoPor' => PersonaFactory::random(),
+           ];
+        });
 
         $manager->flush();
     }
