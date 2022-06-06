37a58bce6c279119c7bcecb53973cdf82d43e78c
David <davitrons@gmail.com>
2022-06-06 21:20:05 +0200

    Arreglado problema sobre las rutas al pasar id de una entidad(framewo… …rk-extra-bundle) que sea un objeto de la entidad y no el id en forma de string

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
